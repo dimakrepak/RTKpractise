a3acbe83e7059d29a6b5d084f75f452724deee4f
Dmitry Krepak <krepakdima@gmail.com>
2021-11-20 19:49:55 +0200

    :smile:

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,13 +1,38 @@
+import { IPost } from "../models/IPost";
 import { postAPI } from "../services/PostService";
 import PostItem from "./PostItem";
 
 export function Post() {
   const { data: posts, error, isLoading } = postAPI.useFetchAllPostsQuery(10);
+  const [createPost, {}] = postAPI.useCreatePostMutation();
+  const [deletePost, {}] = postAPI.useDeletePostMutation();
+  const [updatePost, {}] = postAPI.useUpdatePostMutation();
+
+  const handleCreatePost = async () => {
+    const title = prompt();
+    await createPost({ title, body: title } as IPost);
+  };
+
+  const handleRemove = (post: IPost) => {
+    deletePost(post);
+  };
+  const handleUpdate = (post: IPost) => {
+    updatePost(post);
+  };
   return (
     <>
       {isLoading && <span>Loading...</span>}
       {error && <span>{error}</span>}
-      {posts && posts.map((post) => <PostItem key={post.id} post={post} />)}
+      {posts &&
+        posts.map((post) => (
+          <PostItem
+            remove={handleRemove}
+            update={handleUpdate}
+            key={post.id}
+            post={post}
+          />
+        ))}
+      <button onClick={handleCreatePost}>Add new post</button>
     </>
   );
 }
--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -2,15 +2,26 @@ import { IPost } from "../models/IPost";
 
 interface PostItemProps {
   post: IPost;
+  remove: (post: IPost) => void;
+  update: (post: IPost) => void;
 }
-export default function PostItem({ post }: PostItemProps) {
+export default function PostItem({ post, remove, update }: PostItemProps) {
+  const handleRemove = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    remove(post);
+  };
+  const handleUpdate = () => {
+    const title = prompt() || "";
+    update({ ...post, title });
+  };
   return (
     <>
       <div
+        onClick={handleUpdate}
         style={{ border: "1px solid black", height: "50px", margin: "20px " }}
       >
         {(post.id, post.title)}
-        <button>Delete Post</button>
+        <button onClick={handleRemove}>Delete Post</button>
       </div>
     </>
   );
